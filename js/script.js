e56ca946de87feb3780abaa6170bd57fa9db2c34
Yulia-skh <122690533+Yulia-skh@users.noreply.github.com>
2023-06-25 17:49:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav'),
+    menuItem = document.querySelectorAll('.navigation__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav_active');
+        })
+    })
+})
+
+
+    
+
+   
+ 
+
+    
